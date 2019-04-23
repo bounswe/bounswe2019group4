b33c4896fb4a2ae2b167384e3e67415f090d1df0
Bahadır Hocamoglu <bahadirhocamoglu@gmail.com>
2019-04-23 16:34:11 +0300

    init signup/login layout

---
+++ practice-app/frontend/src/components/login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import "./styles.css";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <p>LoL</p>
+            </div>
+        )
+    }
+}
