6ea4dfe943ee7900f683c5a9e1531dc9e2cecfa2
Bahadır Hocamoglu <bahadirhocamoglu@gmail.com>
2019-04-23 16:34:11 +0300

    init signup/login layout

--- practice-app/frontend/src/App.js
+++ practice-app/frontend/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom";
+
+import Signup from "./components/signup"
+import Login from "./components/login"
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route path="/auth/signup" component={Signup} exact></Route>
+        <Route path="/auth/login" component={Login} exact></Route>
       </div>
     );
   }
---
+++ practice-app/frontend/src/components/signup/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import "./styles.css"
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
