185e03d41aebdbe2c7ee510f4e2cec5c74d348ea
Emre Demircioğlu <44063570+EmreDemircioglu@users.noreply.github.com>
2019-04-30 16:30:11 +0300

    get single exchange rate back-end implementation

--- practice-app/backend/routes/trading-equipments.js
+++ practice-app/backend/routes/trading-equipments.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const secret = require('../secrets')
+
+const alpha = require('alphavantage')({key:secret.alphaKey.key})
 const router = express.Router()
 
 /* 
  * Retrieves a certain exchange rate. Takes two 
  * currencies as parameters with a dash between them
+ * uses alphavantage api
+ * currency parameters should be entered by their fx codes
  */ 
 router.get("/:from-:to", (req, res) => {
     let params = req.params
-    res.send({status: `Wanted to retrieve the rate ${params['from']}/${params['to']}?`})
+  
+    alpha.forex.rate(req.params.from, req.params.to).then(data => {
+        
+        res.send(data['Realtime Currency Exchange Rate']['5. Exchange Rate']);
+      });
 })
 
 // Retrieves all exchange rates the API can
