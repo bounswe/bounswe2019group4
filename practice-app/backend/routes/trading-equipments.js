557d0816f954acb4434f5294a904d10afcd17a46
Emre Demircioğlu <44063570+EmreDemircioglu@users.noreply.github.com>
2019-05-01 13:31:23 +0300

    get full fx data implementation

--- practice-app/backend/routes/trading-equipments.js
+++ practice-app/backend/routes/trading-equipments.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const secret = require('../secrets')
-
+const request= require('request');
 const alpha = require('alphavantage')({key:secret.alphaKey.key})
 const router = express.Router()
 
@@ -15,14 +15,31 @@ router.get("/:from-:to", (req, res) => {
   
     alpha.forex.rate(req.params.from, req.params.to).then(data => {
         res.send({
-            rate: `${params['from']}/${params['to']}?`,
+            rate: `${params['from']}/${params['to']}`,
             value: data['Realtime Currency Exchange Rate']['5. Exchange Rate']});
       });
 })
-
+const getCurrencyLayerApiResponse  = (callback) => {
+    _url_to_get_list_of_fx_data= `http://apilayer.net/api/live?access_key=${secret.currencyLayerKey.key}`;
+    request(_url_to_get_list_of_fx_data, { json: true }, (err, res, body) => {
+    if (err) { 
+        return callback(err);
+     }
+    return callback(body);
+    });
+}
 // Retrieves all exchange rates the API can
 router.get("/$", (req, res) => {
-    res.send({status: `I suppose you wanted to retrieve all exchange rates.`})
+   getCurrencyLayerApiResponse(function(response){
+        if(response["success"]){
+            res.send(response["quotes"]);
+        }else{
+            res.send(response["error"]);
+        }
+   });
+    
 })
 
+
+ 
 module.exports = router
