c0cd3f25c3b5f8bf185127f2130938423de4d123
leventbass <leventbas92@gmail.com>
2019-05-03 22:00:20 +0300

    Update events.js
    Fixed syntax error.

--- practice-app/backend/routes/events.js
+++ practice-app/backend/routes/events.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const url = "https://api.tradingeconomics.com/calendar/country/All/2016-12-02/2016-12-03?c=guest:guest";
 
 // Retrieves some important events that happened recently
-router.get("/list", => (req, res) {
+router.get("/list", (req, res) => {
   request(url, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       const events = JSON.parse(body);
