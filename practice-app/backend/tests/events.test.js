5e7c4f5291ab53382a47a43751ec8167e940a34f
Bahadır Hocamoğlu <bahadirhocamoglu@gmail.com>
2019-05-05 11:23:09 +0300

    Add comments

--- practice-app/backend/tests/events.test.js
+++ practice-app/backend/tests/events.test.js
@@ -3,13 +3,14 @@ const eventsController = require('../controllers/events')
 
 // unit tests on the controller handling GET /events/list requests
 describe('GET /events/list', () => {
-    let response
+    let response        // response of the controller, will be assigned later
+    // prepares testbed, calls the controller and assigns the response to the variable in the closure
     before('log', done => {
         let req = {}    // fake request object
         let res = {     // fake response object with send method
             send: async payload => {
-                response = payload
-                done()
+                response = payload  
+                done()  // calls the next test case
             }
         }
         eventsController.list(req, res) // actual controller call
@@ -17,10 +18,10 @@ describe('GET /events/list', () => {
     // tests whether the list controller returns an array on successful API call
     it('should return an array', done => {
         try {
-            expect(response).to.be.an('Array')
-            done()
-        } catch (error) {
-            done(error)
+            expect(response).to.be.an('Array')  // checks if the response is an array
+            done()          // calls the next test case
+        } catch (error) {   // if error occurs,
+            done(error)     // delegates the error message and continues
         }
     })
     // test whether every object in the response has eventName key
