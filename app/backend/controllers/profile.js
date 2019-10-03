4a86d8e3bc64f15e9055013b4f845042b3277a2f
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-10-03 20:28:25 +0300

    bugfix: profile retrieval returns 500 sometimes

--- app/backend/controllers/profile.js
+++ app/backend/controllers/profile.js
@@ -9,42 +9,25 @@ module.exports.getDetails = async (request, response) => {
   const id = request.params['id']
   const currentUser = request.session['user']
 
-  let user = await User.findOne({ _id : id })  // Retrieve the user instance from database
-
-  if(user == null){
-    response.status(404).send({
-      errmsg: "No such user."
-    })
-    return
-  }
-    
-  if( currentUser == null ){
-    if( user.isPublic ){
-      const { _id, isTrader, isPublic, name, surname, email, location } = user  // Extract certain keys from doc
-      response.send({ _id, isTrader, isPublic, name, surname, email, location });  // Send only the extracted keys    
-    }  else{
-      // WILL BE EDITED AFTER FOLLOW SYSTEM
-        response.status(400).send({
-        errmsg: "User has private profile."
-      })
-    }
-  }
-  else{
-    if( id == currentUser._id ){
-      const { _id, isTrader, isPublic, name, surname, email, location } = user  // Extract certain keys from doc
-      response.send({ _id, isTrader, isPublic, name, surname, email, location });  // Send only the extracted keys 
-    }
-    else{
-      if( user.isPublic ){
-        const { _id, isTrader, isPublic, name, surname, email, location } = user  // Extract certain keys from doc
-        response.send({ _id, isTrader, isPublic, name, surname, email, location });  // Send only the extracted keys    
-      }  else{
-          // WILL BE EDITED AFTER FOLLOW SYSTEM
+  if(currentUser && currentUser._id == id) {  // when the user asks for his own details
+    response.send(currentUser)
+  } else {
+    User.findOne({ _id : id})     // Retrieve the user instance from database
+      .then(user => {             // when the user with given ID exists
+        if(user.isPublic) {       // when the user's data is public 
+          const { _id, isTrader, isPublic, name, surname, email, location } = user    // Extract certain keys from doc
+          response.send({ _id, isTrader, isPublic, name, surname, email, location })  // Send only the extracted keys
+        } else {    // when user's data is private
           response.status(400).send({
-          errmsg: "User has private profile."
+            errmsg: "User has private profile."
+          })
+        }
+      })
+      .catch(error => {   // when it's the case an error occurs accessing the database
+        response.status(404).send({
+          errmsg: "No such user."
         })
-      }
-    }
+      })
   }
 }
 
