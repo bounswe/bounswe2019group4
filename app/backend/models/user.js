52b92b8bd915d389b0680de3369509f60d06cfcb
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-10-03 14:09:32 +0300

    separate database connection

--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -1,23 +1,23 @@
-const mongoose =  require('mongoose');  // The mongodb connector library
+const {mongoose} =  require('../db');  // The mongodb connector library
 
-const {dbCredentials} = require('../secrets.js')  // Database credentials, contact to the team for the actual file
+// const {dbCredentials} = require('../secrets.js')  // Database credentials, contact to the team for the actual file
 
-// it says ensureIndex is deprecated and createIndexes should be used instead
-mongoose.set('useCreateIndex', true)
-// Connecting to database.
-mongoose.connect(
-  `mongodb://${dbCredentials['ip']}/admin`,
-  {
-    useNewUrlParser: true,
-    user: dbCredentials['username'],
-    pass: dbCredentials['password'],
-    useUnifiedTopology: true,
-  },
-  err => {
-    if(err) {
-      console.log(err)
-    }
-  });
+// // it says ensureIndex is deprecated and createIndexes should be used instead
+// mongoose.set('useCreateIndex', true)
+// // Connecting to database.
+// mongoose.connect(
+//   `mongodb://${dbCredentials['ip']}/admin`,
+//   {
+//     useNewUrlParser: true,
+//     user: dbCredentials['username'],
+//     pass: dbCredentials['password'],
+//     useUnifiedTopology: true,
+//   },
+//   err => {
+//     if(err) {
+//       console.log(err)
+//     }
+//   });
 
 /*
   Model for user which has following attributes
