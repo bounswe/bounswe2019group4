c52ce8338ef5878c761b94dde364f90e983ca385
Gürkan Demir <ggurkandemir@gmail.com>
2019-10-03 14:27:56 +0300

    remove redundant codes

--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -1,24 +1,5 @@
 const {mongoose} =  require('../db');  // The mongodb connector library
 
-// const {dbCredentials} = require('../secrets.js')  // Database credentials, contact to the team for the actual file
-
-// // it says ensureIndex is deprecated and createIndexes should be used instead
-// mongoose.set('useCreateIndex', true)
-// // Connecting to database.
-// mongoose.connect(
-//   `mongodb://${dbCredentials['ip']}/admin`,
-//   {
-//     useNewUrlParser: true,
-//     user: dbCredentials['username'],
-//     pass: dbCredentials['password'],
-//     useUnifiedTopology: true,
-//   },
-//   err => {
-//     if(err) {
-//       console.log(err)
-//     }
-//   });
-
 /*
   Model for user which has following attributes
     name,
