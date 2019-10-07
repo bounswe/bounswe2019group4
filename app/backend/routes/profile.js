581a0545885c42fb840c27a90c1b17b84ec2c99d
Gurkan Demir <ggurkandemir@gmail.com>
2019-10-07 20:52:28 +0300

    change endpoint type

--- app/backend/routes/profile.js
+++ app/backend/routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const profileController = require('../controllers/profile')
 
 /*
-  Post endpoint for profile page.
+  Get endpoint for profile page.
   Check controller function for more detail
 */
 router.get('/:id', profileController.getDetails)
