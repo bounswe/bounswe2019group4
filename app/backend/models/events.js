f8bd95180ae1a29dbe31dfd88164bd2f69527e0b
Gürkan Demir <ggurkandemir@gmail.com>
2019-10-09 20:01:25 +0300

    add events model

---
+++ app/backend/models/events.js
@@ -0,0 +1,43 @@
+const {mongoose} =  require('../db');  // The mongodb connector library
+
+let Event = mongoose.model('Event', {
+  calendarId: {
+    type: String
+  },
+
+  event: {
+    type: String
+  },
+
+  date: {
+    type: Date
+  },
+
+  source: {
+    type: String
+  }
+
+  country: {
+    type: String
+  },
+
+  importance: {
+    type: Number
+  },
+
+  actual: {
+    type: Number
+  },
+
+  previous: {
+    type: Number
+  },
+
+  forecast: {
+    type: Number
+  },
+});
+
+module.exports = {
+  Event: Event
+}
