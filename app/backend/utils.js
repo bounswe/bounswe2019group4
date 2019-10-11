acae5e460919f51a58b4bb22d8bc1773b48cc8fd
Gürkan Demir <ggurkandemir@gmail.com>
2019-10-11 20:50:48 +0300

    necessary comments are added

--- app/backend/utils.js
+++ app/backend/utils.js
@@ -1,6 +1,11 @@
 const IBAN = require('iban');
 const commonPassword = require('common-password');
 
+/*
+  Method in order to check whether password is valid or not.
+  Password length must be at least 6 and also,
+  password must not be easy to guess.
+*/
 module.exports.checkPassword = function (password){
   if(password.length < 6)
     return false;
@@ -8,10 +13,19 @@ module.exports.checkPassword = function (password){
   return !(commonPassword(password))
 }
 
+/*
+  Method in order to check whether IBAN is valid or not.
+*/
 module.exports.checkIBAN = function(iban){
   return IBAN.isValid(iban)
 }
 
+/*
+  Method in order to check whether TCKN is valid or not.
+  TCKN length must be 11.
+  Sum of first 10 digits mode 10 must be equals to 11th digit.
+  And 10th digit is tested using TCKN test. 
+*/
 module.exports.checkTCKN = function(value) {
   value = value.toString();
   
