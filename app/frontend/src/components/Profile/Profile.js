327e606fd8e899c9e6463a14c0a71e046709dd6c
Taha Korkmaz <tekorkmaz14@gmail.com>
2019-10-17 16:14:56 +0300

    Added the Profile Page with different styles for both Trader and Public accounts. Added Profile Photo too

--- app/frontend/src/components/Profile/Profile.js
+++ app/frontend/src/components/Profile/Profile.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import { Icon } from 'semantic-ui-react'
 import {loadState} from '../../_core/localStorage'
 import {Form, Checkbox, Grid, Segment, Header, Container, List, Divider} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 
 import history from '../../_core/history';
 import * as userActions from '../../actions/userActions';
+import Image from "semantic-ui-react/dist/commonjs/elements/Image";
+import profilePhoto from "./h2.jpg";
 
 class Profile extends Component {
 
@@ -23,9 +26,12 @@ class Profile extends Component {
 
     render() {
         const { user } = this.state;
+
         return (
             <Segment raised piled padded compact textAlign='left' >
+                <Image src={profilePhoto} size='medium'  floated='center' rounded />
                 <Header textAlign='center'>
+
                 {user.name} {user.surname}
             </Header>
                 <ul >
