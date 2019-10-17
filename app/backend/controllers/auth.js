e2c10e213e6e8d1183046630da2bf44a87c4d703
Gürkan Demir <ggurkandemir@gmail.com>
2019-10-17 23:07:37 +0300

    fix reviews

--- app/backend/controllers/auth.js
+++ app/backend/controllers/auth.js
@@ -1,9 +1,8 @@
-module.exports.authenticationBinder = () => {
-  return (req, res, next) => {
-    if(!req.session['user']) {
-      return res.status(401).send();
-    }
-
-    next()
+module.exports.isAuthenticated = (req, res, next) => {
+  // checks wheter user is logged in or not. if not returns 401.
+  if(!req.session['user']) {
+    return res.status(401).send();
   }
+
+  next()
 }
