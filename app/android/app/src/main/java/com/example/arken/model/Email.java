f64818c1522effc6618089468af5c12ea44d8ec9
Baturalp Yörük <baturalpyoruk1997@hotmail.com>
2019-10-18 23:32:20 +0300

    Create Email class

---
+++ app/android/app/src/main/java/com/example/arken/model/Email.java
@@ -0,0 +1,18 @@
+package com.example.arken.model;
+
+public class Email {
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
