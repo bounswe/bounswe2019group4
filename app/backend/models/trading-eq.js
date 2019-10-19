b34f58e70b8321c37b73b8e6d06c1f9aecd8bb02
Cemal <cemal.aytekin@boun.edu.tr>
2019-10-19 11:25:16 +0300

    create trading-eq model

---
+++ app/backend/models/trading-eq.js
@@ -0,0 +1,48 @@
+const {mongoose} =  require('../db');  // The mongodb connector library
+
+let TradingEquipment = mongoose.model('TradingEquipment', {
+
+  _id: {
+    code:String, 
+    Date:String,
+  },
+  code: {
+    type: String
+  },
+
+  name: {
+    type: String
+  },
+  
+  information: {
+    type: String
+  },
+
+  value: {
+    type: String
+  },
+
+  open: {
+    type: String
+  },
+  high: {
+    type: String
+  },
+  low: {
+    type: String
+  },
+  close: {
+    type: String
+  },
+  volume: {
+    type: String
+  },
+  Date:{
+    type: String
+  }
+  
+});
+
+module.exports = {
+  TradingEquipment: TradingEquipment
+}
