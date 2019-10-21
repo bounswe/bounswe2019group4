8632d43770fc46a7b1a74c0b3dfe1704ef167141
Emre Demircioğlu <44063570+EmreDemircioglu@users.noreply.github.com>
2019-10-21 21:33:43 +0300

    t equipment config

--- app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
+++ app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
@@ -3,7 +3,6 @@ package com.example.arken.fragment;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,8 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -38,13 +35,14 @@ public class BaseFragment extends Fragment {
     private TextView signOutText;
     private Fragment fragment;
     private boolean isLogged = true;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_base, container, false);
         FragmentManager f = getChildFragmentManager();
-        OnBackPressedCallback callback = new OnBackPressedCallback(true ) {
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                if(findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment && isLogged){
+                if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment && isLogged) {
                     AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
                     builder1.setMessage(R.string.log_out_warning);
                     builder1.setCancelable(true);
@@ -68,9 +66,8 @@ public class BaseFragment extends Fragment {
 
                     AlertDialog alert11 = builder1.create();
                     alert11.show();
-                }
-                else{
-                    if(Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
+                } else {
+                    if (Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
                         Navigation.findNavController(signOut).popBackStack();
                 }
             }
@@ -81,10 +78,12 @@ public class BaseFragment extends Fragment {
         eventPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment){
+                if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
                     RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerView);
                     recyclerView.smoothScrollToPosition(0);
-                }else if(findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment){
+                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
+                    findNavController(fragment).popBackStack();
+                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
                     findNavController(fragment).popBackStack();
                 }
             }
@@ -94,7 +93,7 @@ public class BaseFragment extends Fragment {
         tradingEq = view.findViewById(R.id.menu_trading_eq);
         final SharedPreferences prefs = getActivity().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         String email = prefs.getString("email", "default");
-        if(GoogleSignIn.getLastSignedInAccount(getContext()) == null && email.equals("default")){
+        if (GoogleSignIn.getLastSignedInAccount(getContext()) == null && email.equals("default")) {
             signOut.setVisibility(View.GONE);
             signOutText.setVisibility(View.GONE);
             isLogged = false;
@@ -108,21 +107,29 @@ public class BaseFragment extends Fragment {
         tradingEq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if nav is already at trading eq page, then scroll to position 0
-                //else go to trading eq page
+                if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
+                    RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerViewcurrent);
+                    recyclerView.smoothScrollToPosition(0);
+                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
+                    findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
+                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
+                    findNavController(fragment).popBackStack();
+                    findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
+                }
             }
         });
 
         return view;
     }
-    private void signOut(){
+
+    private void signOut() {
         MainActivity.getClient().signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 SharedPreferences.Editor editor = getActivity().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
                 editor.clear();
                 editor.apply();
-                if(Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
+                if (Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
                     Navigation.findNavController(signOut).popBackStack();
 
             }
