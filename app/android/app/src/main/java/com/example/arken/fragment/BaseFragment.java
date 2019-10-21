9b16a46559d06193e321d93ccd2bb734681a8588
elif <elif.caliskan330@gmail.com>
2019-10-21 11:01:34 +0300

    fixed sign out button

--- app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
+++ app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
@@ -8,6 +8,7 @@ import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -16,16 +17,23 @@ import com.example.arken.activity.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
+
 public class BaseFragment extends Fragment {
     private ImageButton eventPage;
     private ImageButton signOut;
+    private Fragment fragment;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_base, container, false);
+        fragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_base_fragment);
         eventPage = view.findViewById(R.id.menu_event);
         eventPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_eventListFragment_self);
+                if(findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment){
+                    findNavController(fragment).navigate(R.id.action_eventListFragment_self);
+
+                }
             }
         });
         signOut = view.findViewById(R.id.menu_logout);
@@ -35,8 +43,9 @@ public class BaseFragment extends Fragment {
                 MainActivity.getClient().signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        if(Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.eventListFragment)
+                        if(Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
                             Navigation.findNavController(signOut).navigate(R.id.action_baseFragment_to_startFragment);
+
                     }
                 });
             }
