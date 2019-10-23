6c4f878ee08ffdf73f755ec958645672808e3ca2
Gürkan Demir <ggurkandemir@gmail.com>
2019-10-23 17:58:27 +0300

    add model for user-follow

---
+++ app/backend/models/user-follow.js
@@ -0,0 +1,17 @@
+const {mongoose} =  require('../db');  // The mongodb connector library
+
+let UserFollow = mongoose.model('UserFollow', {
+  FollowingId: {
+    type: String,
+    require: true
+  },
+
+  FollowedId: {
+    type: String,
+    require: true
+  }
+});
+
+module.exports = {
+  UserFollow: UserFollow
+}
