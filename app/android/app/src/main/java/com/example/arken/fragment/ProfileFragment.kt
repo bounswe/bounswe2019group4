fad9d55522c844d4bd7b718be0f8ddb950383630
Levent <leventbas92@gmail.com>
2019-10-29 02:42:48 +0300

    Bug fix.

---
+++ app/android/app/src/main/java/com/example/arken/fragment/ProfileFragment.kt
@@ -0,0 +1,64 @@
+package com.example.arken.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.arken.R
+import com.example.arken.model.Profile
+import com.example.arken.util.RetroClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var name_textView: TextView
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+        name_textView = view.findViewById(R.id.name_textView)
+
+
+        val call: Call<Profile> = RetroClient.getInstance().apiService.getProfile("5dadf785c0b3f92ab993f5cc")
+
+        call.enqueue(object : Callback<Profile> {
+            override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                if (response.isSuccessful) {
+                    val profile: Profile? = response.body()
+                    val profileName: String? = profile!!.name!!
+
+                   name_textView.text = profileName
+
+                    Toast.makeText(context, profileName, Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(context, response.raw().toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Profile>, t: Throwable) {
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+
+
+
+
+
+        return view
+    }
+}
---
+++ app/android/app/src/main/java/com/example/arken/model/Profile.kt
@@ -0,0 +1,13 @@
+package com.example.arken.model
+
+import java.io.Serializable
+
+class Profile(
+    val _id: String?,
+    val isTrader: Boolean?,
+    val isPublic: Boolean?,
+    val name: String?,
+    val surname: String?,
+    val email: String?,
+    val location: String?
+) : Serializable
