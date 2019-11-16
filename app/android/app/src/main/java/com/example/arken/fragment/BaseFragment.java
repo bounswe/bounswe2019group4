058ced0078bd0301c47831e1c21075b80d2ca706
elif <elif.caliskan330@gmail.com>
2019-11-16 15:29:48 +0300

    search layout

--- app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
+++ app/android/app/src/main/java/com/example/arken/fragment/BaseFragment.java
@@ -20,6 +20,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.arken.R;
 import com.example.arken.activity.MainActivity;
+import com.example.arken.util.MenuAdapter;
+import com.example.arken.util.OnMenuItemClickListener;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,18 +30,14 @@ import static android.content.Context.MODE_PRIVATE;
 import static androidx.navigation.fragment.NavHostFragment.findNavController;
 import static com.example.arken.fragment.LoginFragment.MY_PREFS_NAME;
 
-public class BaseFragment extends Fragment {
-    private ImageButton eventPage;
-    private ImageButton signOut;
-    private ImageButton profileMenu;
-    private ImageButton tradingEq;
-    private TextView signOutText;
-    private TextView profileText;
+public class BaseFragment extends Fragment implements OnMenuItemClickListener {
     private Fragment fragment;
+    private RecyclerView recyclerView;
     private boolean isLogged = true;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_base, container, false);
         FragmentManager f = getChildFragmentManager();
+        recyclerView = view.findViewById(R.id.menu_recycler);
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
@@ -68,80 +66,25 @@ public class BaseFragment extends Fragment {
                     AlertDialog alert11 = builder1.create();
                     alert11.show();
                 } else {
-                    if (Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
-                        Navigation.findNavController(signOut).popBackStack();
+                    if (Navigation.findNavController(recyclerView).getCurrentDestination().getId() == R.id.baseFragment)
+                        Navigation.findNavController(recyclerView).popBackStack();
                 }
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
         fragment = f.findFragmentById(R.id.nav_host_base_fragment);
-        eventPage = view.findViewById(R.id.menu_event);
-        eventPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
-                    RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerView);
-                    recyclerView.smoothScrollToPosition(0);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
-                    findNavController(fragment).popBackStack();
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
-                    findNavController(fragment).navigate(R.id.action_listCurrentFragment_to_eventListFragment);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.profileFragment) {
-                    findNavController(fragment).navigate(R.id.action_profileFragment_to_eventListFragment);
-                }
-            }
-        });
-        signOut = view.findViewById(R.id.menu_logout);
-        signOutText = view.findViewById(R.id.base_signout_text);
-        profileMenu = view.findViewById(R.id.menu_profile);
-        profileText = view.findViewById(R.id.base_profile_text);
-        tradingEq = view.findViewById(R.id.menu_trading_eq);
+
         final SharedPreferences prefs = getActivity().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         String email = prefs.getString("email", "default");
         if (GoogleSignIn.getLastSignedInAccount(getContext()) == null && email.equals("default")) {
-            signOut.setVisibility(View.GONE);
-            signOutText.setVisibility(View.GONE);
-            profileMenu.setVisibility(View.GONE);
-            profileText.setVisibility(View.GONE);
             isLogged = false;
         }
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signOut();
-            }
-        });
-        tradingEq.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
-                    RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerViewcurrent);
-                    recyclerView.smoothScrollToPosition(0);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
-                    findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
-                    findNavController(fragment).popBackStack();
-                    findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.profileFragment) {
 
-                    findNavController(fragment).navigate(R.id.action_profileFragment_to_listCurrentFragment);
-                }
-            }
-        });
+        int[] imageArr = {R.drawable.ic_event, R.drawable.ic_trading_eq, R.drawable.ic_search, R.drawable.ic_person_white, R.drawable.ic_logout};
+        String[] stringArr = {"Events", "Trading Eq","Search", "Profile", "Log Out"};
+        MenuAdapter adapter = new MenuAdapter(imageArr, stringArr, isLogged, this);
 
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
-                   findNavController(fragment).navigate(R.id.action_eventListFragment_to_profileFragment);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
-                    findNavController(fragment).navigate(R.id.action_listCurrentFragment_to_profileFragment);
-                } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
-                    findNavController(fragment).popBackStack();
-                    findNavController(fragment).navigate(R.id.action_eventListFragment_to_profileFragment);
-                }
-            }
-        });
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
@@ -153,10 +96,53 @@ public class BaseFragment extends Fragment {
                 SharedPreferences.Editor editor = getActivity().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
                 editor.clear();
                 editor.apply();
-                if (Navigation.findNavController(signOut).getCurrentDestination().getId() == R.id.baseFragment)
-                    Navigation.findNavController(signOut).popBackStack();
-
+                if (Navigation.findNavController(recyclerView).getCurrentDestination().getId() == R.id.baseFragment)
+                    Navigation.findNavController(recyclerView).popBackStack();
             }
         });
+    }
+
+    @Override
+    public void onMenuItemClicked(int index) {
+        if(index == 0){
+            if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
+                RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerView);
+                recyclerView.smoothScrollToPosition(0);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
+                findNavController(fragment).popBackStack();
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
+                findNavController(fragment).navigate(R.id.action_listCurrentFragment_to_eventListFragment);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.profileFragment) {
+                findNavController(fragment).navigate(R.id.action_profileFragment_to_eventListFragment);
+            }
+        }
+        else if(index == 1){
+            if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
+                RecyclerView recyclerView = fragment.getView().findViewById(R.id.recyclerViewcurrent);
+                recyclerView.smoothScrollToPosition(0);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
+                findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
+                findNavController(fragment).popBackStack();
+                findNavController(fragment).navigate(R.id.action_eventListFragment_to_listCurrentFragment);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.profileFragment) {
+
+                findNavController(fragment).navigate(R.id.action_profileFragment_to_listCurrentFragment);
+            }
+        }
+        else if (index == 3){
+            if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventListFragment) {
+                findNavController(fragment).navigate(R.id.action_eventListFragment_to_profileFragment);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.listCurrentFragment) {
+                findNavController(fragment).navigate(R.id.action_listCurrentFragment_to_profileFragment);
+            } else if (findNavController(fragment).getCurrentDestination().getId() == R.id.eventFragment) {
+                findNavController(fragment).popBackStack();
+                findNavController(fragment).navigate(R.id.action_eventListFragment_to_profileFragment);
+            }
+        }
+        else if (index == 4){
+            signOut();
+        }
+
     }
 }
