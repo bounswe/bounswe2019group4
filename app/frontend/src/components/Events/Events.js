d7b5704feb5d4651459f2d2d320d573d255dfa45
muhammed-kaya-2016400234 <kaya.bera@gmail.com>
2019-11-22 01:32:39 +0300

    Added "filter by date" functionality to events page.Made small adjustments.

--- app/frontend/src/components/Events/Events.js
+++ app/frontend/src/components/Events/Events.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {loadState} from '../../_core/localStorage'
-import {Header, Icon, Pagination, Segment,Dropdown} from 'semantic-ui-react';
+import {Header, Icon, Pagination, Segment,Dropdown,Popup,Button} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import OneStar from '../../assets/onestar.png'
 import TwoStar from '../../assets/twostar.png'
@@ -8,7 +8,8 @@ import ThreeStar from '../../assets/threestar.png'
 import {Link} from 'react-router-dom'
 import * as userActions from '../../actions/userActions';
 import Loading from "../Loading";
-
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
 
 class Events extends Component {
 
@@ -27,14 +28,16 @@ class Events extends Component {
             dropdownItems:[],
             dropdown2Items:[],
             dropdown3Items:[
-                {key:1,value:1,text:1},
-                {key:2,value:2,text:2},
-                {key:3,value:3,text:3}
+                {key:1,value:1,text:"1",image:OneStar},
+                {key:2,value:2,text:"2",image:TwoStar},
+                {key:3,value:3,text:"3",image:ThreeStar}
             ],
             drCo:[],
             drEv:[],
             drImp:[],
-            loading:false
+            loading:false,
+            startDate:new Date(),
+            endDate:new Date()
 
 
 
@@ -50,8 +53,14 @@ class Events extends Component {
         await this.getEvents();
         this.setShownEvents();
         this.sortEventsByDate();
+        await this.setInitialDates();
 
     }
+    setInitialDates=async ()=>{
+        let {0 : a ,length : l, [l - 1] : b} = this.state.events;
+
+        await this.setState({startDate:new Date(b.normalDate),endDate:new Date(a.normalDate)});
+    };
 
     getCountriesForDropdown=()=>{
         let dropdownItems=[];
@@ -139,9 +148,6 @@ class Events extends Component {
             })
         });
     }
-    changeEvents2=async (list)=>{
-        await this.setState({events2:list},()=>this.setState({numPages:Math.floor((this.state.events2.length-1)/this.state.eventPerPage)+1}))
-    };
 
     sortfunc=(f,g)=>{
         let dateDir=this.state.dateDir;
@@ -152,24 +158,11 @@ class Events extends Component {
         if(a.getTime()===b.getTime()) {
             return d-c;
         }
-        /*
-        let a=new Date(f.normalDate);
-        let b=new Date(g.normalDate);
-        let c=f.Importance;
-        let d=g.Importance;
-        if(a.getTime()===b.getTime()){
-
-            if(!impDir){
-                return d-c;
-            }else{
-                return c-d;
-            }
-        }else{
-            return dateDir?a-b:b-a;
-        }*/
         return dateDir?a-b:b-a;
 
     };
+
+    /*
     sortfunc2=(f,g)=>{
         let impDir=this.state.impDir;
         let c=f.Importance;
@@ -180,31 +173,6 @@ class Events extends Component {
             return b-a;
         }
         return impDir?c-d:d-c;
-        /*
-        if(c===d){
-
-            if(!dateDir){
-                return a-b;
-            }else{
-                return b-a;
-            }
-        }else{
-            return impDir?c-d:d-c;
-        }*/
-    };
-    sortEventsByDate=()=>{
-
-        let dateDir=this.state.dateDir;
-        let newevents=this.state.events2;
-        let newevents2=this.state.events;
-        newevents.sort( this.sortfunc);
-        newevents2.sort( this.sortfunc);
-
-        this.setState({events:newevents2});
-        this.setState({events2:newevents});
-        this.setState({dateDir: 1-dateDir});
-        this.setState({impDir:false});
-        this.setState({shownPage:1},this.setShownEvents);
 
     };
 
@@ -225,7 +193,22 @@ class Events extends Component {
         this.setState({dateDir:true});
         this.setState({shownPage:1},this.setShownEvents)
     };
+    */
+    sortEventsByDate=()=>{
 
+        let dateDir=this.state.dateDir;
+        let newevents=this.state.events2;
+        let newevents2=this.state.events;
+        newevents.sort( this.sortfunc);
+        newevents2.sort( this.sortfunc);
+
+        this.setState({events:newevents2});
+        this.setState({events2:newevents});
+        this.setState({dateDir: 1-dateDir});
+        this.setState({impDir:false});
+        this.setState({shownPage:1},this.setShownEvents);
+
+    };
 
     updateDates(){
 
@@ -233,7 +216,6 @@ class Events extends Component {
         //alert(this.state.events.length+"afasfaed")
         let i;
         for(i=0;i<newevents.length;i++) {
-            //alert(newevents[i].Date);
             let d=newevents[i].Date;
             newevents[i].normalDate=normalizeDate(d);
         }
@@ -241,15 +223,8 @@ class Events extends Component {
 
     }
 
-
-
     updatePage= (e,data)=>{
-
-
         this.setState({shownPage:data.activePage},this.setShownEvents);
-
-
-
     };
 
     onDropdownsChange=async()=>{
@@ -262,7 +237,9 @@ class Events extends Component {
             let empty2 = value2.length === 0;
             let empty3 = value3.length === 0;
             for (let i of this.state.events) {
-                if ((value1.includes(i.Country) || empty1) && (value2.includes(i.Event) || empty2)&&(value3.includes(i.Importance)||empty3)) {
+                let date=new Date(i.normalDate);
+
+                if ((value1.includes(i.Country) || empty1) && (value2.includes(i.Event) || empty2)&&(value3.includes(i.Importance)||empty3)&&(compareDates(this.state.startDate,date)&&compareDates(date,this.state.endDate))) {
                     list.push(i);
                 }
             }
@@ -328,6 +305,14 @@ class Events extends Component {
         this.setState({drImp:value},this.onDropdownsChange)
     };
 
+    startDateChange=(date)=>{
+        this.setState({startDate:date},this.onDropdownsChange);
+
+    };
+
+    endDateChange=(date)=>{
+        this.setState({endDate:date},this.onDropdownsChange);
+    };
 
     render() {
         const {shown}  = this.state;
@@ -342,12 +327,13 @@ class Events extends Component {
                     <Segment  raised piled padded compact textAlign='left'>
                         <Header textAlign='center'>
                             Events
+
                         </Header>
                         <table className="ui blue table">
                             <thead>
                             <tr>
                                 <th>
-                                    <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                                    <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
 
                                         Event
 
@@ -364,7 +350,7 @@ class Events extends Component {
                                     </div>
                                 </th>
                                 <th>
-                                    <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                                    <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
 
                                         Country
 
@@ -378,36 +364,65 @@ class Events extends Component {
                                             options={this.state.dropdownItems}
                                             onChange={this.onCountryChange}
                                         />
+
                                     </div>
 
                                 </th>
                                 <th>
+                                    <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
 
-                                    <button style={{backgroundColor:"#fff"}} onClick={this.sortEventsByDate}>
-                                        <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
-                                            <h4>Date
-                                                {this.state.dateDir?
-                                                    (<Icon name={"angle down"}/>):(<Icon name={"angle up"}/>)
-                                                }
-                                            </h4>
-                                        </div>
-                                    </button>
+                                        Date
+                                        <Button.Group style={{marginLeft:6,marginRight:20}}>
+                                        <Button onClick={this.sortEventsByDate}>
+                                            <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
 
+                                                    {this.state.dateDir?
+                                                        (<Icon name={"angle down"}/>):(<Icon name={"angle up"}/>)
+                                                    }
+
+                                            </div>
+                                        </Button>
+
+
+                                        <Popup trigger={
+                                            <Button>
+                                            <Icon name={"filter"}/>
+                                            </Button>
+                                        } flowing hoverable>
+                                            <div style={{display:"flex",flexDirection:"column",alignItems:"center",marginLeft:5}}>
+
+                                                <DatePicker
+                                                    selected={this.state.startDate}
+                                                    onChange={this.startDateChange}
+                                                />
+                                                to
+                                                <DatePicker
+                                                    selected={this.state.endDate}
+                                                    onChange={this.endDateChange}
+                                                />
+                                            </div>
+                                        </Popup>
+                                        </Button.Group>
+                                    </div>
 
                                 </th>
+
                                 <th><h4>Source</h4></th>
                                 <th>
                                     <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
                                         Importance
-                                    <Dropdown
-                                        style={{marginLeft:5}}
-                                        placeholder='All'
-                                        multiple
-                                        search
-                                        selection
-                                        options={this.state.dropdown3Items}
-                                        onChange={this.onImpChange}
-                                    />
+
+                                        <Dropdown
+                                            style={{marginLeft:5}}
+                                            placeholder='All'
+                                            multiple
+                                            search
+                                            selection
+                                            options={this.state.dropdown3Items}
+                                            onChange={this.onImpChange}
+                                        />
+
+
                                     </div>
                                 </th>
                             </tr>
@@ -459,6 +474,7 @@ class Events extends Component {
                         </div>
 
                     </Segment>
+
                 </div>
             ):(<Loading/>)
 
@@ -483,9 +499,36 @@ export function normalizeDate(date){
         year: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
-        hour12: true
+        hour12: false
     };
     return dat.toLocaleDateString('en-US', formatOptions);
+}
+export function compareDates(a,b) { //date a <= date b
+    let c=a.getFullYear();
+    let d=b.getFullYear();
+
+    let m1=a.getMonth();
+    let m2=b.getMonth();
+    let d1=a.getDate();
+    let d2=b.getDate();
+
+
+    if(c>d) {
+
+        return false;
+    }
+    else if(c===d){
+
+        if(m1>m2)
+            return false;
+        else if(m1===m2){
+            if(d1>d2)
+                return false;
+        }
+    }
+    return true;
+
+
 }
 export default connect(null, dispatchToProps)(Events);
 
