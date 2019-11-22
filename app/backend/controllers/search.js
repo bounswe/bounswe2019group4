10851d2a01e270c70488c42f45c8a78452a70cac
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-11-22 15:21:14 +0300

    add comments to search

--- app/backend/controllers/search.js
+++ app/backend/controllers/search.js
@@ -3,8 +3,9 @@ const {filterData} = require('../utils')
 module.exports.search = async (req, res, next) => {
     const {Event, TradingEquipment, Article, User} = req.models
     
-    const terms = req.query.q.split(' ')
+    const terms = req.query.q.split(' ')    // splits query string into words for word-by-word search
 
+    // creates connections for each query
     let usersData = User.find().select('name surname location').lean()
     let eventsData = Event.find().select('Country CalendarId Date Catogory Event').lean()
     let tradingEqData = TradingEquipment.find().select('code name').lean()
@@ -16,6 +17,7 @@ module.exports.search = async (req, res, next) => {
     tradingEqData = await tradingEqData
     articlesData = await articlesData
 
+    // searches for every word in the query string
     const users = terms.flatMap(term => filterData(usersData, ['name', 'surname', 'location'], term))
     const events = terms.flatMap(term => filterData(eventsData, ['Country', 'Catogory', 'Event'], term))
     const tradingEq = terms.flatMap(term => filterData(tradingEqData, ['code', 'name'], term))
