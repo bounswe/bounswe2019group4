4168f66e0a5d842523fc00a176a3623570d35453
elif-caliskan <42263185+elif-caliskan@users.noreply.github.com>
2019-11-25 23:42:08 +0300

    Update SearchUser.kt

--- app/android/app/src/main/java/com/example/arken/fragment/search/SearchUser.kt
+++ app/android/app/src/main/java/com/example/arken/fragment/search/SearchUser.kt
@@ -83,5 +83,6 @@ class SearchUser: Fragment(), OnUserClickedListener {
     fun View.hideKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
+    }
 
 }
