94471a1345ddcd83a1b7768b28bb5e46460d7899
Gürkan Demir <ggurkandemir@gmail.com>
2019-12-06 18:07:13 +0300

    add buy teq endpoint

--- app/backend/controllers/investment.js
+++ app/backend/controllers/investment.js
@@ -2,7 +2,7 @@ const { checkIBAN } = require('../utils')
 
   /*
     Post method for deposit money into account.
-    It returns all articles in database.
+    It adds money to your account.
   */
 module.exports.depositMoney = async (request, response) => {
   let UserAccount = request.models['UserAccount']
@@ -30,6 +30,48 @@ module.exports.depositMoney = async (request, response) => {
     currentAmount = row[currency]
     newAmount = currentAmount + request.body.amount
     UserAccount.updateOne({userId: request.session['user']._id, [currency]: newAmount}).then(async doc => {
+      account = await UserAccount.findOne({userId : request.session['user']._id})
+      return response.status(200).send(account)
+    }).catch(error => {
+      return response.status(400).send(error);
+    })
+  }
+}
+
+
+  /*
+    Post method for buy currency.
+    It buys that currency and adds into your account.
+  */
+module.exports.buy = async (request, response) => {
+  let UserAccount = request.models['UserAccount']
+  let CurrentTradingEquipment = request.models['CurrentTradingEquipment']
+
+  let currency = request.body.currency.toUpperCase()
+
+  row = await UserAccount.findOne({userId : request.session['user']._id})
+  if(!row){
+    return response.status(400).send({
+      errmsg: "User has no account yet!"
+    });
+  } else{
+    let EUR_AMOUNT = row['EUR']
+    let TEQ_AMOUNT = row[currency]
+
+    let currentTeq = await CurrentTradingEquipment.findOne({from: currency, to: 'EUR'})
+    let EXCHANGE_RATE = currentTeq.rate
+    let DECREASE_EUR = EXCHANGE_RATE * request.body.amount
+
+    let FINAL_EUR = EUR_AMOUNT - DECREASE_EUR
+    let FINAL_TEQ = TEQ_AMOUNT + request.body.amount
+
+    if(FINAL_EUR < 0){
+      return response.status(400).send({
+        errmsg: "Not enough money in EUR."
+      })
+    }
+    
+    UserAccount.updateOne({userId: request.session['user']._id, [currency]: FINAL_TEQ, 'EUR': FINAL_EUR}).then(async doc => {
       account = await UserAccount.findOne({userId : request.session['user']._id})
       return response.status(200).send(account)
     }).catch(error => {
--- app/backend/routes/investment.js
+++ app/backend/routes/investment.js
@@ -4,6 +4,7 @@ const investmentController = require('../controllers/investment')
 const {modelBinder, multipleModelBinder} = require('../controllers/db')
 const {isAuthenticated, isTrader} = require('../controllers/auth')
 const { UserAccount } = require('../models/user-account')
+const { CurrentTradingEquipment } = require('../models/current-trading-eq')
 const { validateBody } = require('../controllers/middleware')
 
 /*
@@ -16,6 +17,20 @@ router.post('/deposit', [
   isTrader,
   modelBinder(UserAccount, 'UserAccount')
   ], investmentController.depositMoney)
+
+
+/*
+  Post endpoint for invest for equipment.
+  Check controller function for more detail
+*/
+router.post('/buy', [
+  validateBody(['currency', 'amount']),
+  isAuthenticated,
+  isTrader,
+  multipleModelBinder([
+  [CurrentTradingEquipment, 'CurrentTradingEquipment'],
+  [UserAccount, 'UserAccount']
+])], investmentController.buy)
 
 
 module.exports = router
