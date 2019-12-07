06f0484dc5a1a3996d2cf323abcb9c23b020fa20
Baturalp Yörük <baturalpyoruk1997@hotmail.com>
2019-12-08 02:14:03 +0300

    Make search results of Events clickable

--- app/android/app/src/main/java/com/example/arken/fragment/search/SearchEvent.kt
+++ app/android/app/src/main/java/com/example/arken/fragment/search/SearchEvent.kt
@@ -1,11 +1,15 @@
 package com.example.arken.fragment.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.arken.R
 import com.example.arken.model.Event
@@ -41,12 +45,18 @@ class SearchEvent : Fragment(), OnEventClickedListener {
 
     override fun onItemClicked(event: Event) {
 
-        val call: Call<Event> = RetroClient.getInstance().apiService.getEvent(event._id)
+        val call: Call<Event> = RetroClient.getInstance().apiService.getEvent(event.CalendarId)
 
         call.enqueue(object : Callback<Event> {
             override fun onResponse(call: Call<Event>, response: Response<Event>) {
-                if (response.isSuccessful) {
-
+                if (response.isSuccessful) { //TODO: Here it clicks and get the response but it shows null in fields
+                    recyclerView.hideKeyboard()
+                    val act = SearchFragmentDirections.actionSearchFragmentToEventFragment() // HERE THE DIFFERENCE
+                    //IS actionSearchFragmentToEventFragment method is not taking an argument compared to others.
+                    // The reason is that in event's OnItemClicked method, it gets to the detailed one with
+                    // event parameter on action (act.eventToShow = event) (as seen below), not inside the method...
+                    act.eventToShow = event
+                    Navigation.findNavController(recyclerView).navigate(act)
 
                 } else {
                     Toast.makeText(context, response.message(), Toast.LENGTH_SHORT).show()
@@ -65,6 +75,10 @@ class SearchEvent : Fragment(), OnEventClickedListener {
         }
         eventAdapter!!.dataSet = list
         eventAdapter!!.notifyDataSetChanged()
+    }
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 
 }
