683af982c6803b68dc723f1233a43a05d770507b
Gürkan Demir <ggurkandemir@gmail.com>
2019-12-10 23:56:46 +0300

    add router, controller, and get method for notifications

---
+++ app/backend/routes/notification.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const notificationController = require('../controllers/notification')
+const {modelBinder, multipleModelBinder} = require('../controllers/db')
+const {isAuthenticated} = require('../controllers/auth')
+const { Notification } = require('../models/notification')
+
+/*
+  Get endpoint for notification.
+  Check controller function for more detail
+*/
+router.get('/', [
+  isAuthenticated,
+  modelBinder(Notification, 'Notification')
+], notificationController.getNotifications)
+
+module.exports = router
