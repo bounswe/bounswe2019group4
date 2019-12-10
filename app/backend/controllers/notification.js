44982a93ed031e25156b73666035d4f949b14c0e
Gürkan Demir <ggurkandemir@gmail.com>
2019-12-11 00:08:47 +0300

    add notification when teq level alert happens

--- app/backend/controllers/notification.js
+++ app/backend/controllers/notification.js
@@ -4,7 +4,7 @@
 module.exports.getNotifications = async (request, response) => {
   let Notification = request.models['Notification']
   
-  notifications = await Notification.find({userId: request.session['user']._id})
+  notifications = await Notification.find({userId: request.session['user']._id}).sort({date: -1})
 
   return response.send({notifications})
 }
