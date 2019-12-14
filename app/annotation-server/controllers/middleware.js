898e37aff2e8624cbda7a473496442e3b42969d6
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-12-14 18:55:03 +0300

    set response headers as well in jsonld middleware

--- app/annotation-server/controllers/middleware.js
+++ app/annotation-server/controllers/middleware.js
@@ -1,10 +1,17 @@
 module.exports.jsonld = (req, res, next) => {
-  if (
-    req.headers["content-type"] !=
-    `application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"`
-  ) {
-    res.sendStatus(400);
-  } else {
+  const isCorrectContentType = /application\/ld\+json;\s*profile="http:\/\/www\.w3\.org\/ns\/anno\.jsonld"/.test(
+    req.get("content-type")
+  );
+  if (isCorrectContentType) {
+    res.set("content-type", req.get("content-type"));
+    res.set("Vary", "Accept");
+    res.set(
+      "Link",
+      `Link: <https://${req.hostname}/annotations/>; rel="http://www.w3.org/ns/oa#annotationService"`
+    );
+    res.set("Allow", "GET, OPTIONS, PUT, HEAD, DELETE, PATCH");
     next();
+  } else {
+    res.sendStatus(400);
   }
 };
