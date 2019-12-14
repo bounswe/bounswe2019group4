bd4f77cd28f37ddb005c145bff27913ebee5a255
Gürkan Demir <ggurkandemir@gmail.com>
2019-12-14 14:51:20 +0300

    bugfix teq date format

--- app/backend/utils.js
+++ app/backend/utils.js
@@ -880,7 +880,7 @@ getStocksFromAPI = schedule.scheduleJob('8 23 * * *', function() {
                   }
 
                   teq.rate = temp["4. close"]
-                  teq.Date = day_format
+                  teq.Date = js_yyyy_mm_dd_hh_mm_ss()
 
                   teq.save().then(doc => {
                       
@@ -913,3 +913,14 @@ getStocksFromAPI = schedule.scheduleJob('8 23 * * *', function() {
     start();
   })  
 });
+
+function js_yyyy_mm_dd_hh_mm_ss () {
+  now = new Date();
+  year = "" + now.getFullYear();
+  month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+  day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+  hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+  return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+}
