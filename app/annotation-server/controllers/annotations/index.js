ea2aed830ab5dd736333fb4eb2bb0f897c29510d
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-12-15 18:09:43 +0300

    add delete annotation endpoint

--- app/annotation-server/controllers/annotations/index.js
+++ app/annotation-server/controllers/annotations/index.js
@@ -1,8 +1,10 @@
 const retrieveAnnotation = require("./retrieveAnnotation");
 const createAnnotation = require("./createAnnotation");
 const updateAnnotation = require("./updateAnnotation");
+const deleteAnnotation = require("./deleteAnnotation");
 
 module.exports = {
+  deleteAnnotation,
   retrieveAnnotation,
   updateAnnotation,
   createAnnotation
