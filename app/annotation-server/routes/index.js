0dca759a7b54aadfae525ca96d2e85c28d2d4ca7
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-12-17 09:20:54 +0300

    add 404 endpoint for other endpoints

--- app/annotation-server/routes/index.js
+++ app/annotation-server/routes/index.js
@@ -4,5 +4,7 @@ const annotations = require("./annotations");
 
 const router = Router();
 router.use("/annotations", annotations);
-
+router.use("/", (req, res, next) => {
+  res.sendStatus(400);
+});
 module.exports = router;
