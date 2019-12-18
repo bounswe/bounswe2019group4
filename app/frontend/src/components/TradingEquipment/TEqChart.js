feb8a9aa6065ef6093c7d7b09c1f6436d39b9efa
muhammed-kaya-2016400234 <kaya.bera@gmail.com>
2019-12-18 17:47:47 +0300

    Show daily values of trading equipment on the chart when mouse is over.

--- app/frontend/src/components/TradingEquipment/TEqChart.js
+++ app/frontend/src/components/TradingEquipment/TEqChart.js
@@ -4,13 +4,56 @@ import { scaleTime } from "d3-scale";
 import { utcDay } from "d3-time";
 
 import { ChartCanvas, Chart } from "react-stockcharts";
-import { CandlestickSeries } from "react-stockcharts/lib/series";
+import { CandlestickSeries,LineSeries } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
 import { fitWidth } from "react-stockcharts/lib/helper";
 import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 import { LabelAnnotation, Label, Annotate } from "react-stockcharts/lib/annotation";
 
+import { EdgeIndicator } from "react-stockcharts/lib/coordinates";
 
+import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
+import { HoverTooltip } from "react-stockcharts/lib/tooltip";
+import { ema } from "react-stockcharts/lib/indicator";
+
+import { format } from "d3-format";
+import { timeFormat } from "d3-time-format";
+
+function tooltipContent(ys) {
+    return ({ currentItem, xAccessor }) => {
+        return {
+            x: dateFormat(xAccessor(currentItem)),
+            y: [
+                {
+                    label: "open",
+                    value: currentItem.open && numberFormat(currentItem.open)
+                },
+                {
+                    label: "high",
+                    value: currentItem.high && numberFormat(currentItem.high)
+                },
+                {
+                    label: "low",
+                    value: currentItem.low && numberFormat(currentItem.low)
+                },
+                {
+                    label: "close",
+                    value: currentItem.close && numberFormat(currentItem.close)
+                }
+            ]
+                .concat(
+                    ys.map(each => ({
+                        label: each.label,
+                        value: each.value(currentItem),
+                        stroke: each.stroke
+                    }))
+                )
+                .filter(line => line.value)
+        };
+    };
+}
+const dateFormat = timeFormat("%d.%m.%Y");
+const numberFormat = format(".5f");
 class CandleStickChart extends React.Component {
 
     render() {
@@ -23,6 +66,22 @@ class CandleStickChart extends React.Component {
             xAccessor(data[data.length - 100])
         ];
         const [yAxisLabelX, yAxisLabelY] = [width - margin.left - 40, margin.top + (height - margin.top - margin.bottom) / 2]
+
+        const ema20 = ema()
+            .id(0)
+            .options({ windowSize: 20 })
+            .merge((d, c) => {
+                d.ema20 = c;
+            })
+            .accessor(d => d.ema20);
+
+        const ema50 = ema()
+            .id(2)
+            .options({ windowSize: 50 })
+            .merge((d, c) => {
+                d.ema50 = c;
+            })
+            .accessor(d => d.ema50);
         return (
             <ChartCanvas height={400}
                          ratio={ratio}
@@ -42,9 +101,35 @@ class CandleStickChart extends React.Component {
                            fontSize="12"
                            color="#FFFFFF"
                            text={convertedCurrency} />
+                    <LineSeries
+                        yAccessor={ema20.accessor()}
+                        stroke={ema20.stroke()}
+                    />
+                    <LineSeries
+                        yAccessor={ema50.accessor()}
+                        stroke={ema50.stroke()}
+                    />
                     <CandlestickSeries width={timeIntervalBarWidth(utcDay)} stroke={d => d.close > d.open ? "#6BA583" : "#DB0000"}
                                        wickStroke={d => d.close > d.open ? "#6BA583" : "#DB0000"}
                                        fill={d => d.close > d.open ? "#6BA583" : "#DB0000"}/>
+                    <HoverTooltip
+                        yAccessor={ema50.accessor()}
+                        tooltipContent={tooltipContent([
+                            {
+                                label: `${ema20.type()}(${ema20.options()
+                                    .windowSize})`,
+                                value: d => numberFormat(ema20.accessor()(d)),
+                                stroke: ema20.stroke()
+                            },
+                            {
+                                label: `${ema50.type()}(${ema50.options()
+                                    .windowSize})`,
+                                value: d => numberFormat(ema50.accessor()(d)),
+                                stroke: ema50.stroke()
+                            }
+                        ])}
+                        fontSize={15}
+                    />
                 </Chart>
             </ChartCanvas>
         );
