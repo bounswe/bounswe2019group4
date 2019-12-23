5545e93b6c3edbf126a4248cce90a98c5676b2a1
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-12-23 09:47:24 +0300

    add get annotations by article id endpoint

--- app/annotation-server/controllers/annotations/index.js
+++ app/annotation-server/controllers/annotations/index.js
@@ -1,9 +1,11 @@
+const retrieveAnnotationsByArticle = require("./retrieveAnnotationsByArticle");
 const retrieveAnnotation = require("./retrieveAnnotation");
 const createAnnotation = require("./createAnnotation");
 const updateAnnotation = require("./updateAnnotation");
 const deleteAnnotation = require("./deleteAnnotation");
 
 module.exports = {
+  retrieveAnnotationsByArticle,
   deleteAnnotation,
   retrieveAnnotation,
   updateAnnotation,
--- app/annotation-server/routes/annotations.js
+++ app/annotation-server/routes/annotations.js
@@ -5,11 +5,13 @@ const {
   deleteAnnotation,
   updateAnnotation,
   retrieveAnnotation,
+  retrieveAnnotationsByArticle,
   createAnnotation
 } = require("../controllers/annotations");
 
 const router = Router();
 router.delete("/", deleteAnnotation);
+router.get("/article/:articleId", retrieveAnnotationsByArticle);
 router.get("/:id", retrieveAnnotation);
 router.post("/", validateRequestBody([]), createAnnotation);
 router.put("/", updateAnnotation);
